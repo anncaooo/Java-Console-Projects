20eb2011855b8f4f90cdb917d2c0bd0d2f10c438
anncaooo <119567350+anncaooo@users.noreply.github.com>
2022-12-03 00:18:40 +0800

    Create basic-input-output.java

---
+++ basic-input-output.java
@@ -0,0 +1,12 @@
+//Carol Ann Ursal
+//Date : Dec - 2 - 2022
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter any numbers : ");
+		int num = sc.nextInt();
+		System.out.println("You entered : " + num);
+	}
+}
